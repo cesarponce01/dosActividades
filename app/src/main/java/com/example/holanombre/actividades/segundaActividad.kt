a05cbfdcdf4fdd9272d95b1eddba6eb1a6314c71
clase <cesarinnj01@gmail.com>
2023-11-02 10:07:20 +0100

    02/11/2023

--- app/src/main/java/com/example/holanombre/actividades/segundaActividad.kt
+++ app/src/main/java/com/example/holanombre/actividades/segundaActividad.kt
@@ -6,27 +6,20 @@ import android.widget.TextView
 import com.example.holanombre.R
 import com.example.holanombre.otraCarpeta.Usuario
 
+lateinit var textView2: TextView
 class segundaActividad : AppCompatActivity() {
-
-    lateinit var textView2: TextView
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.segunda_actividad)
-        textView2=findViewById<TextView>(R.id.tvSecond)
 
+        textView2=findViewById(R.id.tvSecond)
         var nombre= intent.getStringExtra("nombre")
         var apellido=intent.getStringExtra("apellido")
-        var bundleUsuario=intent.getStringExtra("claseUsuario")
+
+        var bundleUsuario=intent.getBundleExtra("claseUsuario")
         var usuario= Usuario(bundleUsuario!!)
 
         textView2.text=usuario.nombre+" "+usuario.apellido
-
-
-
     }
-
-
-
 }
